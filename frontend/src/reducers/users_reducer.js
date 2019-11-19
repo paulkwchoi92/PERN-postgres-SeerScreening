bed08ce27905817861db3f856db872f7abf6ec35
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-11-19 15:31:24 -0800

    configure and ready

---
+++ frontend/src/reducers/users_reducer.js
@@ -0,0 +1,24 @@
+import { merge } from "lodash";
+
+const usersReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    // case RECEIVE_USER:
+    //   let result = merge({}, state);
+    //   result[action.user._id] = action.user;
+    //   return result;
+    // case RECEIVE_USERS:
+    //   return merge(
+    //     {},
+    //     state,
+    //     action.users.reduce((pojo, pin) => {
+    //       pojo[pin._id] = pin;
+    //       return pojo;
+    //     }, {})
+    //   );
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
--- queries.js
+++ queries.js
@@ -13,38 +13,42 @@ const pool = new Pool({
 // index query
 const getUsers = (req, res) => {
   pool.query("SELECT * FROM users ORDER BY id ASC", (err, results) => {
-  if (error) {
-    throw error;
-  }
-  res.status(200).json(results.rows);
-});
+    if (err) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
 };
 // for finding a single user, usually on log in
 // $1 will be used as placeholder for PostgresSQL, second argument will be placed in it
-// will be used for "/user/:id"  where :id is the wildcard, a.k.a params 
+// will be used for "/user/:id"  where :id is the wildcard, a.k.a params
 const getUserById = (req, res) => {
-  const id = parseInt(req.params.id)
+  const id = parseInt(req.params.id);
   pool.query("SELECT * FROM users WHERE id = $1", [id], (err, results) => {
-    if (error) {
-      throw error
+    if (err) {
+      throw error;
     }
-    res.status(200).json(results.rows)
-  })
+    res.status(200).json(results.rows);
+  });
 };
 //for posting user or signing up user
 // same concept except name and email will be found from body of the information passed
 // will be used GET/POST request to "/users"
 const createUser = (req, res) => {
-  const { name, email } = request.body
-  pool.query(`INSERT INTO users (name, email) VALUES ($1, $2)`, [name, email], (error, results) => {
-    if (error) {
-      throw error
+  const { name, email } = request.body;
+  pool.query(
+    `INSERT INTO users (name, email) VALUES ($1, $2)`,
+    [name, email],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).send(`User add with IDL ${res.insertID}`);
     }
-    res.status(201).send(`User add with IDL ${res.insertID}`)
-  })
-}
+  );
+};
 
-// for updating user info 
+// for updating user info
 // same concept all $ are place holders,
 // will be used for GET/POST and UPDATE To "/users/:id"
 const updateUser = (request, response) => {
@@ -63,4 +67,22 @@ const updateUser = (request, response) => {
   );
 };
 
-// for deleting user info 
+// for deleting user info
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+};
